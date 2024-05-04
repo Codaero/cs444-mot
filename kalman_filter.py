c69124d3fe0fa22a5aef67fd825d3435f1bbd75f
LohitoBurrito <110351173+LohitoBurrito@users.noreply.github.com>
2024-05-04 01:09:41 -0500

    Kalman Filter Performance Enhancement

--- kalman_filter.py
+++ kalman_filter.py
@@ -12,10 +12,8 @@ class KalmanFilter:
         self.deltaT = 1 / 30
 
         # Measurement Noise Covariance (diagonal elements are covariances)
-        self.R = np.array([[1, 0, 0, 0],
-                           [0, 1, 0, 0],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]])
+        self.R = np.array([[1, 0],
+                           [0, 1]])
 
         # Tune these
         self.process_cv = {
@@ -26,44 +24,42 @@ class KalmanFilter:
         }
         # Process Noise Covariance
         self.Q = np.array([[(self.deltaT ** 4) / 4 * self.process_cv["wx"], 0,
-                            (self.deltaT ** 3) / 2 * self.process_cv["wx"], 0, 0, 0],
+                            (self.deltaT ** 3) / 2 * self.process_cv["wx"], 0],
                            [0, (self.deltaT ** 4) / 4 * self.process_cv["wy"], 0,
-                            (self.deltaT ** 3) / 2 * self.process_cv["wy"], 0, 0],
+                            (self.deltaT ** 3) / 2 * self.process_cv["wy"]],
                            [(self.deltaT ** 3) / 2 * self.process_cv["wx"], 0, self.deltaT ** 2 * self.process_cv["wx"],
-                            0, 0, 0],
+                            0],
                            [0, (self.deltaT ** 3) / 2 * self.process_cv["wy"], 0,
-                            self.deltaT ** 2 * self.process_cv["wy"], 0, 0],
-                           [0, 0, 0, 0, self.process_cv["ww"], 0],
-                           [0, 0, 0, 0, 0, self.process_cv["wh"]]
+                            self.deltaT ** 2 * self.process_cv["wy"]],
                            ])
 
         # State Transition Matrix
-        self.F = np.array([[1, 0, self.deltaT, 0, 0, 0],
-                           [0, 1, 0, self.deltaT, 0, 0],
-                           [0, 0, 1, 0, 0, 0],
-                           [0, 0, 0, 1, 0, 0],
-                           [0, 0, 0, 0, 1, 0],
-                           [0, 0, 0, 0, 0, 1]
+        self.A = np.array([[1, 0, self.deltaT, 0],
+                           [0, 1, 0, self.deltaT],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1],
                            ])
 
+        # Positional Extracter
+        self.C = np.array([[1, 0, 0, 0],
+                           [0, 1, 0, 0]])
         # Measurement Matrix
-        self.H = np.array([[1, 0, 0, 0, 0, 0],
-                           [0, 1, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 1, 0],
-                           [0, 0, 0, 0, 0, 1]
-                           ])
+        self.H = np.array([[1, 0, 0, 0],
+                           [0, 1, 0, 0]])
+
 
     def predict(self, x_curr, P_curr):
-        x_next = self.F @ x_curr.T
-        p_next = self.F @ P_curr @ self.F.T + self.Q
+        x_next = self.A @ x_curr.T
+        p_next = self.A @ P_curr @ self.A.T + self.Q
         return x_next, p_next
 
     def update(self, measurement, x_curr, P_curr):
+        y_k = self.C @ measurement
+
         kalman_gain = P_curr @ self.H.T @ np.linalg.inv(self.H @ P_curr @ self.H.T + self.R)
 
-        new_state = x_curr + kalman_gain @ (measurement - self.H @ x_curr)
+        new_state = x_curr + kalman_gain @ (y_k - self.H @ x_curr)
 
-        p_new = (np.identity(6) - kalman_gain @ self.H) @ P_curr @ (
-                    np.identity(6) - kalman_gain @ self.H).T + kalman_gain @ self.R @ kalman_gain.T
+        p_new = (np.identity(4) - kalman_gain @ self.H) @ P_curr
 
         return new_state, p_new
